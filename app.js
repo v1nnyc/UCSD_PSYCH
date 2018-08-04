acd8919f7a871bb482a1bbcc98eb987d9d6f53dd
Vincent Cannalla <vcannall@ucsd.edu>
2018-08-04 13:44:55 -0700

    added firebase to index

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
+var firebase = require("firebase/app");
 
 var index = require('./routes/index');
 var instructions = require('./routes/instructions')
