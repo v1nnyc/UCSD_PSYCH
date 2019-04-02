e7a6961429d3e9ce4ae9c4490a3a136ba4ba9479
Vincent Cannalla <vincentcannalla@Vincents-MacBook-Pro-2.local>
2019-04-02 13:05:03 -0700

    adding a separate module for firebase initialization

---
+++ public/js/firebae.js
@@ -0,0 +1,13 @@
+// WARNING! THIS SCRIPT MUST BE INCLUDED IN THE HEADERS AFTER THE GOOGLE FIREBASE
+// API SCRIPTS IN THE HEADER TAG!!
+
+var config = {
+  apiKey: "AIzaSyCa5_e0vkkiBXQxKcpvGKHyumnWyvQ_os8",
+  authDomain: "ucsd-psych.firebaseapp.com",
+  databaseURL: "https://ucsd-psych.firebaseio.com",
+  projectId: "ucsd-psych",
+  storageBucket: "ucsd-psych.appspot.com",
+  messagingSenderId: "325328536445"
+};
+
+firebase.initializeApp(config);
