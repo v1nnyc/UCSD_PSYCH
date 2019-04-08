b752eb7eaf122fd929224d9c63ef7900354e5422
Charlotte <cforgeyj@ucsd.edu>
2019-04-08 15:50:25 -0700

    i think it's all working now except db updates

---
+++ public/js/firebae.js
@@ -0,0 +1,65 @@
+// WARNING! THIS SCRIPT MUST BE INCLUDED IN THE HEADERS AFTER THE GOOGLE FIREBASE
+// API SCRIPTS IN THE HEADER TAG!!
+
+var config = {
+  apiKey: "AIzaSyCa5_e0vkkiBXQxKcpvGKHyumnWyvQ_os8",
+  authDomain: "ucsd-psych.firebaseapp.com",
+  databaseURL: "https://ucsd-psych.firebaseio.com",
+  projectId: "ucsd-psych",
+  storageBucket: "ucsd-psych.appspot.com",
+  messagingSenderId: "325328536445"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+function getUserData() {
+  return new Promise(function(resolve) {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        window.userId = user.uid;
+        firebase.database().ref("popup/" + window.userId + "/experiment type")
+          .once('value').then(function(snapshot) {
+            window.experimentType = snapshot.val();
+            resolve()
+          });
+      }
+    });
+  })
+}
+
+//signifies that the current page requires firebase, should pause redirection
+let firebaseInUse;
+
+// used to create a new user
+function createNewUser() {
+  return new Promise(function(resolve, reject) {
+    firebase.auth().signInAnonymously().then(function() {
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          resolve(user.uid)
+        }
+      });
+    });
+  })
+}
+
+// helper function to put data into database
+function setDatabase(dataSet) {
+  return new Promise(function(resolve, reject) {
+    database.ref('popup/' + dataSet.path).set(dataSet.data).then(e => {
+      firebaseCompleted();
+    });
+  });
+}
+
+// setters for firebaseInUse
+function requiresFirebase() {
+  getUserData();
+  firebaseInUse = true;
+}
+
+function firebaseCompleted() {
+  firebaseInUse = false;
+}
